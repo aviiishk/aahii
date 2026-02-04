5812ecc3d9d784db43e73035245f187d247dcf89
avii-e <149656860+avii-e@users.noreply.github.com>
2026-02-04 16:46:46 +0530

    Rename about page, add ImageCarousel component, update navigation

--- src/app/(public)/about/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function AboutPage() {
-  return (
-    <main className="p-10">
-      <h1 className="text-2xl font-semibold">
-       Initialized About Page
-      </h1>
-    </main>
-  );
-}
